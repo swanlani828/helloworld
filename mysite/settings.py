40b3be4cf0ec0ca073a795ba7fbb2a779062e349
Sesha Sai Sreevani Kappagantula <you@example.com>
2020-10-04 18:13:44 -0400

    added static roots

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+django_heroku.settings(locals())
